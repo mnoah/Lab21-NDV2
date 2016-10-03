3fc2a46f9333a90fc0e5c58af70eeb45948291ae
mnoah <noahvrazo@gmail.com>
2016-10-02 23:36:15 -0400

    working

---
+++ Lab21-NDV2/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Lab21_NDV2.Models;
+
+namespace Lab21_NDV2.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index(Response guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                //TODO: Email response to party organizer
+                return View("thanks", guestResponse);
+            }
+            else
+            {
+                //there is a validation error
+                return View();
+            }
+
+
+        }
+        public ViewResult ResponseForm()
+        {
+            return View();
+        }
+    }
+   
+}
